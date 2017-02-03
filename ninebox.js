356dca6401ea9d3a100578aef378a9503623022a
gabrieldaze <gabrieldazevedo@hotmail.com>
2017-02-03 13:33:05 -0200

    Added function to draw dynamic squares

--- ninebox.js
+++ ninebox.js
@@ -35,38 +35,48 @@ class Rect {
 }
 
 class Chart {
-	constructor(context) {
+	constructor(canvas, context) {
+		this.canvas = canvas
 		this.context = context
 		this.rectCollection = []
 		this.verticalPeriod = null
 		this.horizontalPeriod = null
+		this.rectMargin = 0
 	}
-	setVerticalPeriod(start, limit, divided) {
+	setRectMargin(margin) {
+		this.rectMargin = margin
+	}
+	setVerticalPeriod(start, limit, count) {
 		this.verticalPeriod = {
 			'start':start,
 			'limit':limit,
-			'divided':divided
+			'count':count
 		}
 	}
-	setHorizontalPeriod(start, limit, divided) {
+	setHorizontalPeriod(start, limit, count) {
 		this.horizontalPeriod = {
 			'start':start,
 			'limit':limit,
-			'divided':divided
+			'count':count
 		}
 	}
-	getCountRect() {
-		return horizontalPeriod.divided * verticalPeriod.divided
+	getRectSize() {
+		return {
+			'width':this.canvas.width / this.horizontalPeriod.count - this.rectMargin,
+			'height':this.canvas.height / this.verticalPeriod.count - this.rectMargin
+		}
 	}
-	addRect(rectInstance) {
-		this.rectCollection.push(rectInstance)
-	}
-	drawStaticRect() {
-		this.rectCollection.forEach(function(rect) {
-			rect.drawOnPos(rect.x, rect.y, rect.width, rect.height)
-		});
-	}
-	drawRect() {
-		
+	drawRectList(color) {
+		this.context.fillStyle = color
+		var currentY = 0
+		for(var i = 0; i < this.verticalPeriod.count; i++) {
+			var currentX = 0
+			for(var j = 0; j < this.horizontalPeriod.count; j++) {
+				this.context.fillRect(currentX, currentY, this.getRectSize().width, this.getRectSize().height)
+				currentX = currentX + this.getRectSize().width + this.rectMargin
+			}
+			console.log('fillRect: ' + currentX + ' - ' + currentY)
+			currentY = currentY + this.getRectSize().height + this.rectMargin
+		}
 	}
 }
--- usage.js
+++ usage.js
@@ -1,3 +1,7 @@
 canvas = document.getElementById("canvas")
 context = canvas.getContext('2d')
-chart = new Chart(context)
+chart = new Chart(canvas, context)
+chart.setRectMargin(5)
+chart.setVerticalPeriod(0, 100, 5)
+chart.setHorizontalPeriod(0, 100, 5)
+chart.drawRectList('black')
