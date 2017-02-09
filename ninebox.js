82dcb2b7b42def0b7003b94e6d814243a6a742f8
Gabriel Azevedo <gabriel.azevedo@artedigitalinternet.com.br>
2017-02-09 14:11:27 -0200

    Removed unnecessary code

--- ninebox.js
+++ ninebox.js
@@ -1,39 +1,3 @@
-class Point {
-	constructor(x, y) {
-		this.x = x
-		this.y = y
-	}
-}
-
-class Rect {
-	constructor(width, height) {
-		this.width = width
-		this.height = height
-		this.static = false
-		this.x = null
-		this.y = null
-	}
-	drawOnPos(context, point, color) {
-		context.fillStyle = color
-		context.fillRect(point.x, point.y, this.width, this.height)
-	}
-	drawOnCenter(context, point, color) {
-		context.fillStyle = color
-		context.fillRect(point.x - this.width / 2, point.y - this.height / 2, this.width, this.height)
-	}
-	setStaticPosition(point) {
-		this.static = true
-		this.x = point.x
-		this.y = point.y
-	}
-	getCenter() {
-		return {
-			'x':this.width / 2,
-			'y':this.height / 2
-		};
-	}
-}
-
 class Chart {
 	constructor(potentialString, performanceString) {
 
